01e51d5ddad34ac86bd835c54a626c701486f8e2
andrewandrewxu <andrewandrewqq@gmail.com>
2017-07-28 13:44:56 -0700

    1st change

--- tests/test_worker_basic.py
+++ tests/test_worker_basic.py
@@ -8,39 +8,6 @@ from workers.basic_worker import BasicUserParseWorker
 
 class TestWorkerBasic(unittest.TestCase):
 
-"""
-    def test_basic_worker_connection(self):
-        
-        Purpose: Test regular running of worker
-        Expectation: startup system, hit the reddit user and parse the data, fail to send to mothership (exception)
-
-        :precondition: Mothership server not running
-        :return:
-        
-        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
-	ConnectionRefusedError = "failed"
-        # Can't connect to mother, so should raise ConnectionRefusedError, but should run everything else
-        self.assertRaises(ConnectionRefusedError, worker.run)
-
-    def test_worker_parsing(self):
-        
-        Purpose: Test regular parsing mechanisms of worker
-        Expectation: Load html file, send it to worker to parse, should return list of results
-
-        :return:
-        
-        worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
-        file_path = '%s/%s' % (os.path.dirname(os.path.realpath(__file__)), 'test_resources/sample_GET_response.html')
-
-        with codecs.open(file_path, encoding='utf-8') as f:
-            text = f.read()
-
-        results, next_page = worker.parse_text(str(text).strip().replace('\r\n', ''))
-
-        self.assertGreater(len(results), 0)     # Check that results are returned
-        self.assertEqual(len(results[0]), 3)    # Check that results are in triplets (check formatting)
-"""
-
     def test_worker_add_links_max_limit(self):
         worker = None
         worker = BasicUserParseWorker("https://www.reddit.com/user/Chrikelnel")
